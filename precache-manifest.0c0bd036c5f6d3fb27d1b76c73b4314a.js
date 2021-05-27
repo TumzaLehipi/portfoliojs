c087d1cc3fc0e84c92cef467d96db09bd80a94ce
tumzalehipi <brianmoipolai1@gmail.com>
2021-05-27 20:09:45 +0200

    Updates

---
+++ precache-manifest.0c0bd036c5f6d3fb27d1b76c73b4314a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b9ebccdbbb3bbc0effd1980f5f5b0c7",
+    "url": "/styled-comp-reactjs-portfolio/index.html"
+  },
+  {
+    "revision": "d227d2c8cc2869698bf6",
+    "url": "/styled-comp-reactjs-portfolio/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "4f817126753e9646c475",
+    "url": "/styled-comp-reactjs-portfolio/static/js/2.e0710b07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/styled-comp-reactjs-portfolio/static/js/2.e0710b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d227d2c8cc2869698bf6",
+    "url": "/styled-comp-reactjs-portfolio/static/js/main.6be7eb85.chunk.js"
+  },
+  {
+    "revision": "cd193214fd51d544b71c",
+    "url": "/styled-comp-reactjs-portfolio/static/js/runtime-main.29a04918.js"
+  },
+  {
+    "revision": "9e9988a62307fb83666428b9f9d7605f",
+    "url": "/styled-comp-reactjs-portfolio/static/media/about.9e9988a6.svg"
+  },
+  {
+    "revision": "cd6f09b9b6be79f892983898d445d033",
+    "url": "/styled-comp-reactjs-portfolio/static/media/contact.cd6f09b9.svg"
+  },
+  {
+    "revision": "11a706e8bf04f97366f1c7d6f1dcfd3f",
+    "url": "/styled-comp-reactjs-portfolio/static/media/facebook.11a706e8.svg"
+  },
+  {
+    "revision": "6ed3db481fc0668d4efbe2e1e887b8bd",
+    "url": "/styled-comp-reactjs-portfolio/static/media/github.6ed3db48.svg"
+  },
+  {
+    "revision": "a273e999e11e6e649a3296c4e2f9be16",
+    "url": "/styled-comp-reactjs-portfolio/static/media/home.a273e999.svg"
+  },
+  {
+    "revision": "201afcb070acc5707cf61ffba66865d9",
+    "url": "/styled-comp-reactjs-portfolio/static/media/instagram.201afcb0.svg"
+  },
+  {
+    "revision": "083538a628e5ba64621ed9d3547077e2",
+    "url": "/styled-comp-reactjs-portfolio/static/media/linkedin.083538a6.svg"
+  },
+  {
+    "revision": "3dbc4fe5a44cd2134659b257474df5f2",
+    "url": "/styled-comp-reactjs-portfolio/static/media/profilepic.3dbc4fe5.jpg"
+  },
+  {
+    "revision": "48771fda0560ea23e6db25cac8eeadd1",
+    "url": "/styled-comp-reactjs-portfolio/static/media/skills.48771fda.svg"
+  }
+]);
